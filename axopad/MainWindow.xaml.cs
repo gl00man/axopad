440fa9a1269742e487597db8383020cf44cac76b
gl00man <maciekbereda46@gmail.com>
2020-11-21 02:09:11 +0100

    saving, opening, saving as files working

--- axopad/MainWindow.xaml.cs
+++ axopad/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,135 @@ namespace axopad
     /// </summary>
     public partial class MainWindow : Window
     {
+        string filePath = "";
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        /*
+         * BUTTONS EVENTS
+         */
+
+        private void newFileBtn_Click(object sender, RoutedEventArgs e)
+        {
+            filePath = "";
+            mainTxt.Document.Blocks.Clear();
+        }
+
+        private void openFileBtn_Click(object sender, RoutedEventArgs e)
+        {
+            filePath = GetFilePath();
+            OpenFile(filePath);
+        }
+
+        private void saveFileBtn_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFile(false, filePath);
+        }
+
+        private void saveAsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFile(true, GetToSavePath());
+        }
+
+        private void findPhraseBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void helpBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        /*
+         * SAVING AND READING DATA
+         */
+
+        private void SaveFile(bool saveAs, string path)
+        {
+            if(saveAs)
+            {
+                TextRange range;
+                FileStream fStream;
+
+                range = new TextRange(mainTxt.Document.ContentStart, mainTxt.Document.ContentEnd);
+
+                fStream = new FileStream(path, FileMode.Create);
+                range.Save(fStream, DataFormats.Text);
+                fStream.Close();
+            }
+            else if(filePath != "" && !saveAs)
+            {
+                MessageBox.Show("e");
+                TextRange range;
+                FileStream fStream;
+
+                range = new TextRange(mainTxt.Document.ContentStart, mainTxt.Document.ContentEnd);
+
+                fStream = new FileStream(path, FileMode.Create);
+                range.Save(fStream, DataFormats.Text);
+                fStream.Close();
+            }
+        }
+
+        private void OpenFile(string path)
+        {
+            TextRange range;
+
+            FileStream fStream;
+
+            if (File.Exists(path))
+            {
+
+                range = new TextRange(mainTxt.Document.ContentStart, mainTxt.Document.ContentEnd);
+
+                fStream = new FileStream(path, FileMode.OpenOrCreate);
+                range.Load(fStream, DataFormats.Text);
+                fStream.Close();
+            }
+            else
+            {
+                MessageBox.Show("Can't find file!");
+            }
+        }
+        
+        /*
+         * GETTING FILES PATH
+         */
+
+        private string GetFilePath()
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Multiselect = false;
+            fileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            fileDialog.DefaultExt = ".txt";
+            Nullable<bool> dialogOK = fileDialog.ShowDialog();
+
+            if (dialogOK == true)
+            {
+                string filePath = "";
+                foreach (string x in fileDialog.FileNames)
+                {
+                    filePath += x;
+                }
+                return filePath;
+            }
+            return null;
+        }
+
+        private string GetToSavePath()
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.DefaultExt = "txt";
+            sfd.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            string sfdname = sfd.FileName;
+            if (sfd.ShowDialog() == (DialogResult == true))
+            {
+                System.IO.Path.GetFileName(sfd.FileName);
+            }
+            return System.IO.Path.GetFullPath(sfd.FileName);
         }
     }
 }
